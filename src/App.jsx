41691e64a628698562c96ea5b571051f3ab78de0
AhmedAfzal2 <ahmedafzal6705@gmail.com>
2025-12-08 00:07:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ReservationPage from "./pages/ReservationPage";
-import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/reservations" element={<ReservationPage />} />
-        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   );
