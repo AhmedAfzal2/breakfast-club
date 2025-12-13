8ab9056ef1cb45d60935005611aa56fd96c39d2c
anoosheheh <159456937+anoosheheh@users.noreply.github.com>
2025-12-13 22:22:15 +0500

    Created Hero Section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import HeroSection from "../components/HomePage/HeroSection";
 
 function HomePage() {
   return (
     <Layout>
-      {/* Home page content will go here */}
+      <div className="homepage-container">
+      <HeroSection />
+      {/* Future sections will go here: */}
+      {/* <DishesSection /> */}
+      {/* <ReviewsSection /> */}
+    </div>
     </Layout>
   );
 }
 
 export default HomePage;
-
