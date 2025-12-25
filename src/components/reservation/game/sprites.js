90473610a2103363c31b7397eda9b1271af47559
AhmedAfzal2 <ahmedafzal6705@gmail.com>
2025-12-25 19:09:51 +0500

    sprite improvements

--- src/components/reservation/game/sprites.js
+++ src/components/reservation/game/sprites.js
@@ -1,27 +1,38 @@
 const sprites = {
   top: {
     stand: "/assets/images/bob/up_stand.png",
-    walk: ["/assets/images/bob/up_walk.png", "/assets/images/bob/up_walk2.png"],
+    walk: [
+      "/assets/images/bob/up_walk1.png",
+      "/assets/images/bob/up_stand.png",
+      "/assets/images/bob/up_walk2.png",
+      "/assets/images/bob/up_stand.png",
+    ],
   },
   bottom: {
     stand: "/assets/images/bob/down_stand.png",
     walk: [
-      "/assets/images/bob/down_walk.png",
+      "/assets/images/bob/down_walk1.png",
+      "/assets/images/bob/down_stand.png",
       "/assets/images/bob/down_walk2.png",
+      "/assets/images/bob/down_stand.png",
     ],
   },
   left: {
     stand: "/assets/images/bob/left_stand.png",
     walk: [
-      "/assets/images/bob/left_walk.png",
+      "/assets/images/bob/left_walk1.png",
+      "/assets/images/bob/left_stand.png",
       "/assets/images/bob/left_walk2.png",
+      "/assets/images/bob/left_stand.png",
     ],
   },
   right: {
     stand: "/assets/images/bob/right_stand.png",
     walk: [
-      "/assets/images/bob/right_walk.png",
+      "/assets/images/bob/right_walk1.png",
+      "/assets/images/bob/right_stand.png",
       "/assets/images/bob/right_walk2.png",
+      "/assets/images/bob/right_stand.png",
     ],
   },
 };
