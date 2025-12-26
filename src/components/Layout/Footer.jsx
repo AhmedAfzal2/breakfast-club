06887e062cb6a47133538a39fc9e500e32cf1a14
AhmedAfzal2 <ahmedafzal6705@gmail.com>
2025-12-26 19:47:40 +0500

    footer swipe

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 const breakfastClubLogo = "/assets/images/breakfast_club_logo.png";
+import useSwipeDown from "../cart/useSwipeDown";
 import {
   Instagram,
   Linkedin,
@@ -17,6 +18,8 @@ import {
 function Footer() {
   const [isOpen, setIsOpen] = useState(false);
   const footerRef = useRef(null);
+
+  useSwipeDown(footerRef, toggleFooter);
 
   const toggleFooter = () => {
     setIsOpen(!isOpen);
@@ -50,87 +53,95 @@ function Footer() {
         </div>
         <div className="footer-content">
           <div className="footer-top">
-        <div className="footer-logo">
-          <img src={breakfastClubLogo} alt="Breakfast Club Logo" />
-        </div>
-        <div className="footer-line"></div>
-        <div className="footer-links">
-          <Link to="/menu">Menu</Link>
-          <Link to="/">About us</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/reservations">Reservations</Link>
-        </div>
-      </div>
-      <div className="footer-heading">
-        <h1>EXPLORE OUR SERVICES!</h1>
-      </div>
-      <div className="footer-tools">
-        <Link to="/menu" className="tool-card">
-          <div className="tool-text">
-            <span>
-              <strong style={{ color: "var(--china-rose)" }}>ORDER</strong> now!
-            </span>
-            <p>
-              Check out our delicious menu with everything ranging from sweet to
-              savoury breakfast items.
-            </p>
+            <div className="footer-logo">
+              <img src={breakfastClubLogo} alt="Breakfast Club Logo" />
+            </div>
+            <div className="footer-line"></div>
+            <div className="footer-links">
+              <Link to="/menu">Menu</Link>
+              <Link to="/">About us</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/reservations">Reservations</Link>
+            </div>
           </div>
-          <div className="tool-image">
-            <img
-              src="assets/images/menu-items/breakfast/hot-breakfast/pancake.png"
-              alt="Order Food"
-            />
+          <div className="footer-heading">
+            <h1>EXPLORE OUR SERVICES!</h1>
           </div>
-        </Link>
-        <Link to="/menu" className="tool-card">
-          <div className="tool-text">
-            <span>
-              <strong style={{ color: "var(--china-rose)" }}>BEVERAGES</strong>{" "}
-              for you
-            </span>
-            <p>
-              Enjoy our selection of fresh juices, aromatic coffees, and
-              refreshing teas to complement your meal.
-            </p>
+          <div className="footer-tools">
+            <Link to="/menu" className="tool-card">
+              <div className="tool-text">
+                <span>
+                  <strong style={{ color: "var(--china-rose)" }}>ORDER</strong>{" "}
+                  now!
+                </span>
+                <p>
+                  Check out our delicious menu with everything ranging from
+                  sweet to savoury breakfast items.
+                </p>
+              </div>
+              <div className="tool-image">
+                <img
+                  src="assets/images/menu-items/breakfast/hot-breakfast/pancake.png"
+                  alt="Order Food"
+                />
+              </div>
+            </Link>
+            <Link to="/menu" className="tool-card">
+              <div className="tool-text">
+                <span>
+                  <strong style={{ color: "var(--china-rose)" }}>
+                    BEVERAGES
+                  </strong>{" "}
+                  for you
+                </span>
+                <p>
+                  Enjoy our selection of fresh juices, aromatic coffees, and
+                  refreshing teas to complement your meal.
+                </p>
+              </div>
+              <div className="tool-image">
+                <img
+                  src="assets\images\menu-items\drinks\refreshing_drinks\sharbat.png"
+                  alt="Beverages"
+                />
+              </div>
+            </Link>
+            <Link to="/reservations" className="tool-card">
+              <div className="tool-text">
+                <span>
+                  <strong style={{ color: "var(--china-rose)" }}>
+                    RESERVE
+                  </strong>{" "}
+                  now!
+                </span>
+                <p>
+                  Book a table in advance to ensure you have a spot at the
+                  popular breakfast club.
+                </p>
+              </div>
+              <div className="tool-image">
+                <img src="/assets/images/hen.png" alt="Reserve Table" />
+              </div>
+            </Link>
           </div>
-          <div className="tool-image">
-            <img src="assets\images\menu-items\drinks\refreshing_drinks\sharbat.png" alt="Beverages" />
-          </div>
-        </Link>
-        <Link to="/reservations" className="tool-card">
-          <div className="tool-text">
-            <span>
-              <strong style={{ color: "var(--china-rose)" }}>RESERVE</strong>{" "}
-              now!
-            </span>
-            <p>
-              Book a table in advance to ensure you have a spot at the popular
-              breakfast club.
-            </p>
-          </div>
-          <div className="tool-image">
-            <img src="/assets/images/hen.png" alt="Reserve Table" />
-          </div>
-        </Link>
-      </div>
 
-      <div className="footer-bottom">
-        <div className="footer-legal">
-          <a href="#legal">Legal Center</a>
-          <a href="#status">Status</a>
+          <div className="footer-bottom">
+            <div className="footer-legal">
+              <a href="#legal">Legal Center</a>
+              <a href="#status">Status</a>
+            </div>
+            <div className="footer-line"></div>
+            <div className="footer-social">
+              <Instagram size={20} />
+              <Linkedin size={20} />
+              <Twitter size={20} />
+              <Youtube size={20} />
+              <Facebook size={20} />
+              <Github size={20} />
+            </div>
+          </div>
         </div>
-        <div className="footer-line"></div>
-        <div className="footer-social">
-          <Instagram size={20} />
-          <Linkedin size={20} />
-          <Twitter size={20} />
-          <Youtube size={20} />
-          <Facebook size={20} />
-          <Github size={20} />
-        </div>
-      </div>
-      </div>
-    </footer>
+      </footer>
     </>
   );
 }
